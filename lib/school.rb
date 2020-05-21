6602e03494e8f7c3b5ed2dea5ce2a561cf3b692f
Tyler Firth <tyfirth@gmail.com>
2020-05-21 22:58:43 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,17 +1,11 @@
 class School
   
-  attr_accessor :name, :roster
+  attr_accessor :roster
   
   def initialize(name)
     @name = name
+    @roster
   end
   
-  def name= (name)
-    @name = name
-  end
-  
-  def name
-    @name
-  end
   
 end
